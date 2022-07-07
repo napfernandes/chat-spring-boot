f05fe5c5d9efb889f65a8929dce29b3fcee3a697
Nicolas Fernandes <nicolaspfernandes@outlook.com>
2022-07-07 09:28:58 +0200

    Adding Mongo configuration bean.

---
+++ src/main/java/com/napfernandes/chat/config/MongoConfig.java
@@ -0,0 +1,30 @@
+package com.napfernandes.chat.config;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.data.mongodb.MongoDatabaseFactory;
+import org.springframework.data.mongodb.core.convert.DbRefResolver;
+import org.springframework.data.mongodb.core.convert.DefaultDbRefResolver;
+import org.springframework.data.mongodb.core.convert.DefaultMongoTypeMapper;
+import org.springframework.data.mongodb.core.convert.MappingMongoConverter;
+import org.springframework.data.mongodb.core.mapping.MongoMappingContext;
+
+@Configuration
+public class MongoConfig {
+    @Autowired
+    private MongoMappingContext mongoMappingContext;
+
+    @Autowired
+    private MongoDatabaseFactory mongoDatabaseFactory;
+
+    @Bean
+    public MappingMongoConverter mappingMongoConverter() {
+        DbRefResolver dbRefResolver = new DefaultDbRefResolver(mongoDatabaseFactory);
+        MappingMongoConverter converter = new MappingMongoConverter(dbRefResolver, mongoMappingContext);
+
+        converter.setTypeMapper(new DefaultMongoTypeMapper(null));
+
+        return converter;
+    }
+}
